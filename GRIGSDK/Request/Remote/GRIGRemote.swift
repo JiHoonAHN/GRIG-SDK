9712b744b18c036393732446135f81dda3d8a449
JiHoonAHN <ahnjh1028@naver.com>
2022-09-07 20:00:20 +0900

    [UPDATE] GRIG Run Script

--- GRIGSDK/Request/Remote/GRIGRemote.swift
+++ GRIGSDK/Request/Remote/GRIGRemote.swift
@@ -1,26 +1,15 @@
 import Foundation
 import Apollo
 
-final class GithubRemote {
+final class GRIGRemote {
     //MARK: - Properties
-    static let shared = GithubRemote()
-
-    private let client: ApolloClient = {
-        let url = URL(string: "https://api.github.com/graphql")!
-        let store = ApolloStore(cache: InMemoryNormalizedCache())
-        let config = URLSessionConfiguration.default
-        config.httpAdditionalHeaders = ["Authorization": "bearer \(ProcessInfo.processInfo.environment["GITHUB"] ?? "")"]
-        let session = URLSessionClient(sessionConfiguration: config, callbackQueue: nil)
-        let networkProvider = NetworkInterceptorProvider(store: store, client: session)
-        let transport = RequestChainNetworkTransport(interceptorProvider: networkProvider, endpointURL: url)
-        return ApolloClient(networkTransport: transport, store: store)
-    }()
+    static let shared = GRIGRemote()
     
-    func fetchRankList() {
+    func fetchUserInfo() {
         
     }
     
-    func fetchGenerationList() {
+    func fetchUserTotalContribution(login: String){
         
     }
 }
