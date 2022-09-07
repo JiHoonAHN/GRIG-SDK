b1d1fa5d457f4d227264edb9cc715be32e527258
JiHoonAHN <ahnjh1028@naver.com>
2022-09-07 09:56:05 +0900

    [CREATE] run script

---
+++ GRIGSDK/Request/Remote/GRIGRemote.swift
@@ -0,0 +1,8 @@
+//
+//  GRIGRemote.swift
+//  GRIGSDK
+//
+//  Created by Ji-hoon Ahn on 2022/09/07.
+//
+
+import Foundation
---
+++ GRIGSDK/Request/Remote/GithubRemote.swift
@@ -0,0 +1,8 @@
+//
+//  GithubRemote.swift
+//  GRIGSDK
+//
+//  Created by Ji-hoon Ahn on 2022/09/07.
+//
+
+import Foundation
