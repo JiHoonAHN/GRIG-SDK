5d195ccc776856224acfd51817be5c9d3562457f
JiHoonAHN <ahnjh1028@naver.com>
2022-09-13 15:14:59 +0900

    [CREATE] SPM

---
+++ Package.swift
@@ -0,0 +1,20 @@
+// swift-tools-version: 5.6
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "GRIG-SDK",
+    platforms: [.iOS(.v9)],
+    products: [
+        .library(
+            name: "GRIG-SDK",
+            targets: ["GRIGSDK"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/apollographql/apollo-ios", branch: "main")
+    ],
+    targets: [
+        .binaryTarget(name: "GRIGSDK", path: "./Sources/GRIGSDK.xcframework"),
+    ]
+)
