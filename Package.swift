03469ef8d013eed9ae0ddb1becf45198527a5d52
안지훈 <ahnjh1028@naver.com>
2022-10-26 07:54:05 +0900

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "GRIG-SDK",
-    platforms: [.iOS(.v9)],
+    platforms: [.iOS(.v12)],
     products: [
         .library(
             name: "GRIG-SDK",
